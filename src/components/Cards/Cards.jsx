901f1b01e2711f41238cf967482847c1d3bc8986
Abhidb2007 <abhishekdb2210@gmail.com>
2026-07-28 21:53:55 +0530

    added code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -17,7 +17,7 @@ function Cards() {
               <h3>{card.title}</h3>
               <p>{card.description}</p>
               <button className={styles.CardButton} type="button">
-                {card.button}
+                {card.Button}
               </button>
             </motion.article>
           ))}
