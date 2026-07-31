8e13fd27ed876b7743c9adb25fb319c5217aa030
Abhidb2007 <abhishekdb2210@gmail.com>
2026-07-31 23:13:40 +0530

    code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,7 +11,7 @@ function Cards() {
             <motion.article
               key={card.title}
               className={styles.card}
-              whileHover={{ y: -10 }}
+              whileHover={{ y: -11 }}
               transition={{ duration: 0.22, ease: 'easeOut' }}
             >
               <h3>{card.title}</h3>
